a1b9ac8191a26321b0fde3c9e7931a9ad3122e7e
1129-tame <onemanlive0603tame@yahoo.ne.jp>
2021-03-31 16:38:31 +0900

    環境変数化

--- app/functions.php
+++ app/functions.php
@@ -4,14 +4,16 @@ function str2html(string $string) :string {
 }
 //関数作成、XSS対策、特殊文字の無害化
 function db_open() :PDO {  //型宣言でPDO型を指定
-    $user = "phpuser";
-    $password = "uRZ89Tfw32H2PG9r";
+    $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+    $dsn = "mysql:host={$db['DB_HOST']};dbname={$db['DB_DATABASE']};charset=utf8";
+    $user = $db['DB_USERNAME'];
+    $password = $db['DB_PASSWORD'];
     $opt = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_EMULATE_PREPARES => false,
         PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
     ]; //DSN データソース名
-    $dbh = new PDO('mysql:host=localhost:3308;dbname=kuchikomi;charset=utf8', $user, $password, $opt);
+    $dbh = new PDO($dsn, $user, $password, $opt);
     return $dbh; //返り値を返す
 }
 
