c6e56cb630de7007cfcfcd8cf87660120aa59bfe
1129-tame <onemanlive0603tame@yahoo.ne.jp>
2021-04-01 01:02:42 +0900

    composer

---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b9c07ab880f068df5769f56c4fc0c0df12c2a3ad',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9c07ab880f068df5769f56c4fc0c0df12c2a3ad',
+    ),
+  ),
+);
