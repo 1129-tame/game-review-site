657ffd9de7e5c8be7a4da0d90a9487f3de42a7b5
tame <78590091+1129-tame@users.noreply.github.com>
2021-05-13 16:56:33 +0900

    コミット

--- app/token_check.php
+++ app/token_check.php
@@ -2,11 +2,11 @@
 if (!isset($_SESSION)) {
     session_start();
 }
-if (empty($_POST['token']) && empty($_POST['token'])) {
+if (empty($_POST['token']) && empty($_GET['token'])) {
     echo "エラーが発生しました。";
     exit;
 }
-if (!(hash_equals($_SESSION['token'], $_POST['token'])) && !(hash_equals($_SESSION['token'], $_POST['token']))) {
+if (!(hash_equals($_SESSION['token'], $_POST['token'])) && !(hash_equals($_SESSION['token'], $_GET['token']))) {
     echo "エラーが発生しました。（２）";
     exit;
 }
