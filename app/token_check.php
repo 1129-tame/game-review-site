28869f782a984711459f1f8267249880212668d3
tame <78590091+1129-tame@users.noreply.github.com>
2021-05-13 16:05:41 +0900

    トークンのエラー修正

--- app/token_check.php
+++ app/token_check.php
@@ -2,11 +2,11 @@
 if (!isset($_SESSION)) {
     session_start();
 }
-if (empty($_GET['token'])) {
+if (empty($_GET['token']) && empty($_POST['token'])) {
     echo "エラーが発生しました。";
     exit;
 }
-if (!(hash_equals($_SESSION['token'], $_GET['token']))) {
+if (!(hash_equals($_SESSION['token'], $_GET['token'])) && !(hash_equals($_SESSION['token'], $_POST['token']))) {
     echo "エラーが発生しました。（２）";
     exit;
 }
